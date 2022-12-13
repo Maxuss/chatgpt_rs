cc74ee465d2c2c98f0d473d18bed26df398edcb2
maxus <maxus.info.mail@gmail.com>
2022-12-13 17:26:44 +0300

    Make authentication data fields public

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,6 @@
 /// Contains the authentication data, can be received from the different auth methods
 pub struct AuthenticationData {
-    pub(crate) cf_token: String,
-    pub(crate) user_agent: String,
-    pub(crate) access_token: String,
+    pub cf_token: String,
+    pub user_agent: String,
+    pub access_token: String,
 }
