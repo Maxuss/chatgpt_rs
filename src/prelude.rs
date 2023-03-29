2aad8253f156b46ae6d0bdc8d57930bcf1ab46d6
maxus <maxus.info.mail@gmail.com>
2023-03-29 22:06:14 +0300

    Some examples

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,7 @@
 pub use crate::client::ChatGPT;
+pub use crate::config::ModelConfiguration;
 pub use crate::converse::Conversation;
+#[cfg(feature = "streams")]
+pub use crate::types::ResponseChunk;
 pub use crate::types::{ChatMessage, MessageChoice, TokenUsage};
 pub use crate::Result;
