41a27dd2add873bf14794da46ff9f6915904a411
maxus <maxus.info.mail@gmail.com>
2023-07-27 18:30:19 +0300

    some docs

--- src/functions/traits.rs
+++ src/functions/traits.rs
@@ -3,11 +3,15 @@ use schemars::JsonSchema;
 
 // deserialize -> parsing result from API
 // schema -> providing description
+/// This trait represents an object containing ChatGPT function arguments.
+/// To use this trait, just derive [JsonSchema] and [Deserialize]
 pub trait FunctionArgument<'de>: Deserialize<'de> + JsonSchema {}
 
 impl<'de, T: Deserialize<'de> + JsonSchema> FunctionArgument<'de> for T {}
 
+/// This trait represents a struct containing actual ChatGPT function handling logic
 #[async_trait::async_trait]
 pub trait CallableAsyncFunction<A> {
+    /// Invokes this function. This method should not be called outside of internal logic.
     async fn invoke(arguments: A);
 }
--- src/functions/types.rs
+++ src/functions/types.rs
@@ -4,10 +4,14 @@ use serde::ser::SerializeStruct;
 use serde::{Serialize, Serializer};
 use std::marker::PhantomData;
 
+/// A descriptor containing information about a ChatGPT function
 #[derive(Debug, Clone)]
 pub struct FunctionDescriptor<'a, A: FunctionArgument<'a>> {
+    /// Contains the name of the function, by which it will be called, e.g. `my_function`
     pub name: &'a str,
+    /// Describes what this function does. Description should clearly state what this function does so ChatGPT understands when to call it.
     pub description: &'a str,
+    /// Phantom data used for referencing the type of parameter object.
     pub parameters: PhantomData<A>,
 }
 
@@ -25,8 +29,11 @@ impl<'a, A: FunctionArgument<'a>> Serialize for FunctionDescriptor<'a, A> {
     }
 }
 
+/// This struct represents a ChatGPT function.
 #[derive(Debug, Clone)]
 pub struct GptFunction<'a, A: FunctionArgument<'a>, C: CallableAsyncFunction<A>> {
+    /// Descriptor for this function. See [FunctionDescriptor] fields for details
     pub descriptor: FunctionDescriptor<'a, A>,
+    /// Phantom data used for referencing the handler for this function. See [CallableAsyncFunction] for details.
     pub callable: PhantomData<C>,
 }
